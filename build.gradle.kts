93d81d83aa537a0a04c0b94997d93ce31ea5e964
MasonTW <mingxin.wang@thoughtworks.com>
2022-03-25 14:45:35 +0800

    [mingxin] chore:add new dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,7 +25,10 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-mustache")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 }
 
 tasks.withType<KotlinCompile> {
